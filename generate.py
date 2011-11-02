9bba3c39d7ae17355c9679059c81b67666deb12d
Jarno Elovirta <jarno@elovirta.com>
2011-11-02 09:29:27 +0200

    Add test case generation script.

---
+++ generate.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import sys
+import os
+import os.path
+
+def main(id, input, types):
+    casedir = os.path.join(os.path.abspath("."), "testcase", id)
+    if not os.path.exists(casedir):
+        os.makedirs(casedir)
+    case = open(os.path.join(casedir, "build.xml"), "w")
+    try:
+        case.write("""<?xml version="1.0" encoding="UTF-8"?>
+<project name="%(id)s" default="all">
+  <property name="dita.dir" location="${basedir}/../.."/>
+  <target name="all" depends="%(default)s"/>\n""" % { "id": id, "default": ", ".join([id + "." + t for t in types]) })
+        for t in types:
+            __vals = { "id": id, "type": t, "input": input }
+            case.write("""  <target name="%(id)s.%(type)s" if="run.%(type)s" unless="skip.%(type)s">
+    <ant antfile="${dita.dir}/build.xml">\n""" % __vals)
+            if t == "preprocess":
+                case.write("""      <target name="build-init"/>
+      <target name="preprocess"/>\n""")
+            else:
+                case.write("""      <property name="transtype" value="%(type)s"/>\n""" % __vals)
+            case.write("""      <property name="args.input" location="${dita.dir}/testdata/${ant.project.name}/%(input)s"/>
+      <property name="output.dir" location="${dita.dir}/testresult/${ant.project.name}/%(type)s"/>
+      <property name="dita.temp.dir" location="${dita.dir}/temp/${ant.project.name}/%(type)s"/>
+    </ant>
+  </target>\n""" % __vals)
+        case.write("""</project>""" % __vals)
+    finally:
+        case.close
+    datadir = os.path.join(os.path.abspath("."), "testdata", id)
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+
+default_types = ["xhtml", "eclipsehelp", "eclipsecontent", "javahelp", "htmlhelp", "pdf", "troff", "docbook", "wordrtf", "odt"]
+valid_types = ["preprocess"]
+valid_types.extend(default_types)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print "Usage: generate.py ID INPUT [TYPES]"
+        sys.exit(1)
+    __id = sys.argv[1]
+    __input = sys.argv[2]
+    if len(sys.argv) > 3:
+        __types = sys.argv[3:]
+    else:
+        __types = default_types
+    for __t in __types:
+        if not __t in valid_types:
+            print "Error: Type %s is not a valid type" % __t
+            sys.exit(1)
+    main(__id, __input, __types)
